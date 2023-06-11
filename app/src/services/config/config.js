3fbd114222baecc43c435c5045cf24c26e868f52
Nilo Mendonca <nilojrmendonca@gmail.com>
2023-06-11 16:06:13 -0300

    Add homolog address

--- app/src/services/config/config.js
+++ app/src/services/config/config.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const SetupBackEnd = axios.create({
     // baseURL: 'https://localhost:5000'
-    baseURL: 'https://192.168.0.109:5000'
+    // baseURL: 'https://192.168.0.109:5000'
+    baseURL: 'https://18.222.114.102:5000'
 });
 
 export default SetupBackEnd;
