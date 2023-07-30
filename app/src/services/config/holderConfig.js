3ef2bdb6ebf8554b7961210d3af6cf09c3371c5a
Nilo Mendonca <nilojrmendonca@gmail.com>
2023-07-30 17:09:02 -0300

    Update base URL

--- app/src/services/config/holderConfig.js
+++ app/src/services/config/holderConfig.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const SetupHolder = axios.create({
-    baseURL: 'https://localhost:5001'
-    // baseURL: 'https://3.145.67.169:5001/'
-    // baseURL: 'https://192.168.0.109:5001'
-    // baseURL: 'https://18.222.114.102:5001'
+    // baseURL: 'https://localhost:5001'
+    baseURL: 'https://146.190.157.57:5001/'
 });
 
 // "start": "export PORT=443&&export HTTPS=true&&SSL_CRT_FILE=cert.pem&&SSL_KEY_FILE=key.pem react-scripts start",
